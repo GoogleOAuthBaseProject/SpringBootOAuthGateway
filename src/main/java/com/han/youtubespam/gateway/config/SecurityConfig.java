0f4b6bdda4cd28ed5482148f5f133d6dc17d6d92
PromotezCitizen <gkstkdgus821@gmail.com>
2026-02-28 01:18:05 +0900

    hotfix] 회원탈퇴를 security로 이관 및 401 발생 시 redirect 방지

--- src/main/java/com/han/youtubespam/gateway/config/SecurityConfig.java
+++ src/main/java/com/han/youtubespam/gateway/config/SecurityConfig.java
@@ -11,12 +11,14 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfigurationSource;
 
+import com.han.youtubespam.gateway.consts.CookieConstant;
 import com.han.youtubespam.gateway.filter.JwtAuthenticationFilter;
 import com.han.youtubespam.gateway.filter.OAuthContextFilter;
 import com.han.youtubespam.gateway.handler.OAuth2LoginFailureHandler;
 import com.han.youtubespam.gateway.handler.OAuth2LoginSuccessHandler;
 import com.han.youtubespam.gateway.resolver.JwtAuthorizationRequestResolver;
 
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 
 @Configuration
@@ -41,6 +43,12 @@ public class SecurityConfig {
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		http.csrf(AbstractHttpConfigurer::disable)
 			.formLogin(AbstractHttpConfigurer::disable)
+			.logout(logout -> logout.logoutUrl("/auth/signout")
+				.invalidateHttpSession(true)
+				.deleteCookies("JSESSIONID", CookieConstant.REFRESH_TOKEN)
+				.clearAuthentication(true)
+				.logoutSuccessHandler(
+					(request, response, authentication) -> response.setStatus(HttpServletResponse.SC_NO_CONTENT)))
 			.httpBasic(AbstractHttpConfigurer::disable)
 			.requestCache(AbstractHttpConfigurer::disable)
 			.cors(cors -> cors.configurationSource(corsConfigurationSource))
@@ -58,6 +66,11 @@ public class SecurityConfig {
 					endpoint -> endpoint.authorizationRequestResolver(oAuthRequestResolver))
 				.successHandler(oAuth2LoginSuccessHandler)
 				.failureHandler(oAuth2LoginFailureHandler)
+			)
+			.exceptionHandling(exception -> exception
+				.authenticationEntryPoint((request, response, authException) -> {
+					response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+				})
 			);
 		return http.build();
 	}
